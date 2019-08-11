8cb8d41c59ce7982a811c720e8ee4b77c9161bab
Mike Harsch <mike@harschsystems.com>
2019-08-11 13:12:53 -0600

    add missing constructor for DAQC2plate

--- plates.js
+++ plates.js
@@ -1,11 +1,13 @@
 var RELAYplate = require('./RELAYplate');
 var DAQCplate = require('./DAQCplate');
+var DAQC2plate = require('./DAQC2plate');
 var MOTORplate = require('./MOTORplate');
 var THERMOplate = require('./THERMOplate');
 
 module.exports = {
     RELAYplate: RELAYplate,
     DAQCplate: DAQCplate,
+    DAQC2plate: DAQC2plate,
     MOTORplate: MOTORplate,
     THERMOplate: THERMOplate
 }
